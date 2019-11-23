27d939deeca74aa0a2dd4c2ba9d9f0e2dac90be5
Abel <abe2211@gmail.com>
2019-11-23 02:03:05 -0600

    Added routes and 3 new screens.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import MainLayout from './containers/MainLayout';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <MainLayout />
-    </div>
+    <Router>
+      <div className="App">
+        <MainLayout />
+      </div>
+    </Router>
   );
 }
 
--- src/containers/MainContent.js
+++ src/containers/MainContent.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import IconColumns from '../components/IconColumns';
+import AboutUs from '../components/AboutUs';
+import Shop from './Shop';
+import ContactUs from './ContactUs';
 import '../styles/main.css';
+
+const Home = () => {
+  return (
+    <>
+      <div className="banner" />
+      <IconColumns />
+    </>
+  );
+};
 
 const MainContent = () => {
   return (
     <div className="main-content">
-      <div className="banner" />
-      <IconColumns />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/shop" component={Shop} />
+        <Route path="/contact" component={ContactUs} />
+        <Route path="/about" component={AboutUs} />
+      </Switch>
     </div>
   );
 };
